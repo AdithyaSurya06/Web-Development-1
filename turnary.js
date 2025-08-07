9699b5c9a8638cb1c7a4587e00924de3c37b0e66
AdithyaSurya06 <suryaadithya9849@gmail.com>
2025-08-07 10:08:37 +0530

    Add files via upload

---
+++ turnary.js
@@ -0,0 +1,21 @@
+let score=60;
+let grade=(score>=90)?"A":
+          (score>=80)?"B": 
+          (score>=70)?"C":"F";
+    console.log(grade);
+
+let age=20;
+// let name=prompt("enter ur name");
+let message=(age>=18)?"u are an adult":"u are not an adult";
+console.log(message);
+
+//both r equal
+
+if(age>=18)
+{
+    console.log("adult");
+}
+else
+{
+    console.log("minor");
+}
